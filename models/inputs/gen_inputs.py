67c6b3453cec06ec871b31240ba300b77ded3ef1
Jess Mankewitz <jmankewitz@gmail.com>
2024-09-26 10:48:01 -0500

    add code and model outputs

--- models/inputs/gen_inputs.py
+++ models/inputs/gen_inputs.py
@@ -5,13 +5,13 @@ chainNum = 0
 
 freqs = ['HF', 'LF']
 test_angles = np.linspace(0, .5, num=11).round(3).tolist()
-lf_costs = np.arange(1, 3*8 , 3).tolist()
+lf_costs = np.arange(1, 4.5, .5).tolist()
 hf_costs = [1]
-noise = [.1, .25, .5]
+noise = [.1, .15,.2, .25]
 
-combos = itertools.product(freqs, freqs, freqs, lf_costs, hf_costs, noise, test_angles)
+combos = itertools.product(['HF'], ['LF'], ['HF'], lf_costs, hf_costs, noise, test_angles)
 
-with open('inputs/input_grid.csv', 'w') as csv_file :
+with open('input_grid.csv', 'w') as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
     for freq1, freq2, freq3, lf_cost, hf_cost, noise, test_angle in combos:
         writer.writerow([chainNum, freq1, freq2, freq3, lf_cost, hf_cost, noise, test_angle])
